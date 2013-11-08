e5f1797c1ef612e3067411b0422e8ecc83cd45d5
zhixing <zhixing135@hotmail.com>
2013-11-09 02:08:33 +0800

    Completed part one: training program

--- training.c
+++ training.c
@@ -14,17 +14,91 @@
 #define NUM_OF_ROUNDS 900
 #define NUM_OF_PLAYERS 5
 
+#define INITIAL_BALL_POSITION_X 64
+#define INITIAL_BALL_POSITION_Y 32
+
+#define MPI_BUFFER_SIZE 128
+
+#define MAX_DISTANCE_PER_ROUND 10
+#define MIN_DISTANCE_PER_ROUND 1
+
 typedef struct{
 	int x;
 	int y;
 } Position;
 
 typedef struct{
-	Position position;
+	Position lastPosition;
+	Position currentPosition;
+	int isBallReached;
+	int isBallWinned;
 	int runnedDistance;
-	int countTouchBall;
-	int countWin;
+	int countReachBall;
+	int countWinBall;
 } Player;
+
+typedef struct{
+	Position currentPosition;
+	Position lastPosition;
+} Ball;
+
+/*
+* Run towards the ball.
+* If able to reach, reach. If not, go all the way horizontally,
+* and then vertically.
+*/
+void runTowardsBall(Player *self, Ball ball, int distanceInCurrentRound){
+
+	int distance = getDistance(ball.currentPosition, (*self).currentPosition);
+	
+	if (distance <= distanceInCurrentRound){
+		(*self).isBallReached = 1;
+		(*self).runnedDistance += distance;
+		(*self).countReachBall++;
+		(*self).currentPosition.x = ball.currentPosition.x;
+		(*self).currentPosition.y = ball.currentPosition.y;
+	}
+
+	else{
+		(*self).isBallReached = 0;
+		(*self).runnedDistance += distanceInCurrentRound;
+
+		int distanceX = ball.currentPosition.x - (*self).currentPosition.x;
+		int distanceY = ball.currentPosition.y - (*self).currentPosition.y;
+		int directionX = distanceX >= 0 ? 1 : -1;
+		int directionY = distanceY >= 0 ? 1 : -1;
+		
+		if (distanceInCurrentRound >= abs(distanceX)){
+			(*self).currentPosition.x = ball.currentPosition.x;
+			(*self).currentPosition.y = (*self).currentPosition.y + directionY * (distanceInCurrentRound - abs(distanceX));
+		} else if (distanceInCurrentRound >= abs(distanceY)){
+			(*self).currentPosition.x = (*self).currentPosition.x + directionX * (distanceInCurrentRound - abs(distanceY));
+			(*self).currentPosition.y = ball.currentPosition.y;
+		} else{
+			int stepHorizontal = getRandomNumberWithinRange(0, abs(distanceInCurrentRound));
+			(*self).currentPosition.x = (*self).currentPosition.x + directionX * stepHorizontal;
+			(*self).currentPosition.y = (*self).currentPosition.y + directionY * (distanceInCurrentRound - stepHorizontal);			
+		}
+	}
+
+	//printf("In runTowardsBall function. Ball: (%d, %d). Step: %d. Original: (%d, %d). New: (%d, %d)\n", ball.currentPosition.x, ball.currentPosition.y, distanceInCurrentRound, (*self).lastPosition.x, (*self).lastPosition.y, (*self).currentPosition.x, (*self).currentPosition.y);
+}
+
+
+/*
+* Return a random integer in the range [start, end], inclusively.
+*/
+int getRandomNumberWithinRange(int start, int end){
+	return rand() % (end - start + 1) + start;
+}
+
+int getDistance(Position p, Position q){
+	return abs(p.x - q.x) + abs(p.y - q.y);
+}
+
+/*	
+* Main Function:
+*/
 
 int main(int argc, char *argv[]){
 	int rank, numOfTasks;
@@ -41,13 +115,158 @@ int main(int argc, char *argv[]){
 	}
 
 	// Set a random seed:
-	srand(time(NULL) + rand());
+	srand(time(NULL) + rank * 100);
+
+	// Information:
+	Ball ball;
+	Player self;
+
+	// Auxiliary Variables:
+	int receiveBuffer[MPI_BUFFER_SIZE];
+	int sendBuffer[MPI_BUFFER_SIZE];
 
 	for (currentRound = 0; currentRound < NUM_OF_ROUNDS; currentRound++){
+		// Field process:
+		if(rank == FIELD_PROCESS){
+			if(currentRound == 0){
+				// Initialize and broadcast the location of ball:
+				ball.currentPosition.x = INITIAL_BALL_POSITION_X;
+				ball.currentPosition.y = INITIAL_BALL_POSITION_Y;
+			}
+
+			ball.lastPosition = ball.currentPosition;
+
+			// Mark A. Broadcast the ball's location.
+			sendBuffer[0] = ball.currentPosition.x;
+			sendBuffer[1] = ball.currentPosition.y;
+			MPI_Bcast(&sendBuffer, 2, MPI_INT, FIELD_PROCESS, MPI_COMM_WORLD);
+
+			// Mark B. Gather info about whether any player has reached the ball
+			MPI_Gather(&sendBuffer, 1, MPI_INT, &receiveBuffer, 1, MPI_INT, FIELD_PROCESS, MPI_COMM_WORLD);
+
+			int reachCount = 0;
+			int reachedPlayers[NUM_OF_PLAYERS]; // e.g. [0, 2, 3] means rank 0, 2 and 3 has reached.
+			int i;
+			for (i = 0; i < NUM_OF_PLAYERS; i++){
+				if (receiveBuffer[i] == 1){
+					reachedPlayers[reachCount++] = i;
+				}
+			}
+
+			// If nobody has reached the ball:
+			int winnerRank;
+			if (reachCount == 0){
+				winnerRank = FIELD_PROCESS;	
+			} else if (reachCount == 1){
+				winnerRank = reachedPlayers[0];
+			} else{
+				int winnerIndex = getRandomNumberWithinRange(0, reachCount - 1);
+				winnerRank = reachedPlayers[winnerIndex];
+				sendBuffer[0] = winnerRank;
+			}
+			// Mark C. Tell the players who win.
+			sendBuffer[0] = winnerRank;
+			MPI_Bcast(&sendBuffer, 1, MPI_INT, FIELD_PROCESS, MPI_COMM_WORLD);
+
+			// Mark D. Receive the new location of the ball.
+			MPI_Scatter(&sendBuffer, 2, MPI_INT, &receiveBuffer, 2, MPI_INT, winnerRank, MPI_COMM_WORLD);
+			if (reachCount != 0){
+				//printf("In field process: detected %d people reach in the next round! And rank %d wins and kicks the ball to: (%d, %d)\n",reachCount, winnerRank, receiveBuffer[0], receiveBuffer[1]);
+				ball.currentPosition.x = receiveBuffer[0];
+				ball.currentPosition.y = receiveBuffer[1];
+			}
+
+			// Mark E. Gather updates from each one.
+			MPI_Gather(&sendBuffer, 9, MPI_INT, &receiveBuffer, 9, MPI_INT, FIELD_PROCESS, MPI_COMM_WORLD);
+			
+			// Print-out required by Homework:
+
+			printf("%d\n", currentRound);
+			printf("%d %d\n", ball.currentPosition.x, ball.currentPosition.y);
+			int j;
+			for (j = 0; j < NUM_OF_PLAYERS; j++){
+				int k;
+				printf("%d ", j);
+				for (k = 0; k < 9; k++){
+					printf("%d ", receiveBuffer[j * 9 + k]);
+				}
+				printf("\n");
+			}
+			printf("\n");
+
+			// Print-out with details:
+			// printf("\n\nRound %d\n", currentRound);
+			// printf("Ball's location: (%d, %d) ==> (%d, %d)\n", ball.lastPosition.x, ball.lastPosition.y, ball.currentPosition.x, ball.currentPosition.y);
+			// int j;
+			// for (j = 0; j < NUM_OF_PLAYERS; j++){
+			// 		printf("\nRank: %d\n", j);
+			// 		printf("Location: (%d, %d) ==> (%d, %d)\n", receiveBuffer[j * 9 + 0], receiveBuffer[j * 9 + 1], receiveBuffer[j * 9 + 2], receiveBuffer[j * 9 + 3]);
+			// 		printf("Reached? %d\n", receiveBuffer[j * 9 + 4]);
+			// 		printf("Winned? %d\n", receiveBuffer[j * 9 + 5]);
+			// 		printf("Total Running: %d\n", receiveBuffer[j * 9 + 6]);
+			// 		printf("Reach Count: %d\n", receiveBuffer[j * 9 + 7]);
+			// 		printf("Win Count: %d\n", receiveBuffer[j * 9 + 8]);
+			// }
+			// printf("\n");
+		} 
+
+		// Player process:
+		else{
+			if(currentRound == 0){
+				self.currentPosition.x = getRandomNumberWithinRange(0, FIELD_LENGTH);
+				self.currentPosition.y = getRandomNumberWithinRange(0, FIELD_WIDTH);
+				self.runnedDistance = 0;
+				self.countReachBall = 0;
+				self.countWinBall = 0;
+			}
+
+			self.lastPosition = self.currentPosition;
+
+			// Mark A. Respond to the field's broadcast of ball location:
+			MPI_Bcast(&receiveBuffer, 2, MPI_INT, FIELD_PROCESS, MPI_COMM_WORLD);
+			ball.currentPosition.x = receiveBuffer[0];
+			ball.currentPosition.y = receiveBuffer[1];
+
+			//printf("Received ball location: (%d, %d)\n", ball.currentPosition.x, ball.currentPosition.y);
+
+			int ditanceInCurrentRound = getRandomNumberWithinRange(MIN_DISTANCE_PER_ROUND, MAX_DISTANCE_PER_ROUND);
+			runTowardsBall(&self, ball, ditanceInCurrentRound);
+
+			// Mark B. Send the info about whether I've reached the ball:
+			sendBuffer[0] = self.isBallReached;
+			MPI_Gather(&sendBuffer, 1, MPI_INT, &receiveBuffer, 1, MPI_INT, FIELD_PROCESS, MPI_COMM_WORLD);
+
+			// Mark C. Receive the information about who wins.
+			MPI_Bcast(&receiveBuffer, 1, MPI_INT, FIELD_PROCESS, MPI_COMM_WORLD);
+			int winnerRank = receiveBuffer[0];
+			
+			// If I win:
+			if (winnerRank == rank){
+
+				self.isBallWinned = 1;
+				self.countWinBall++;
+
+				// Kick the ball to a random location:
+				sendBuffer[FIELD_PROCESS * 2] = getRandomNumberWithinRange(0, FIELD_LENGTH);
+				sendBuffer[FIELD_PROCESS * 2 + 1] = getRandomNumberWithinRange(0, FIELD_WIDTH);
+
+				// Mark D. Winner sends the new location.
+				MPI_Scatter(&sendBuffer, 2, MPI_INT, &receiveBuffer, 2, MPI_INT, winnerRank, MPI_COMM_WORLD);
+				
+			} else{
+				self.isBallWinned = 0;
+				// Mark D. Non-Winner receives the scatter and do nothing.
+				MPI_Scatter(&sendBuffer, 2, MPI_INT, &receiveBuffer, 2, MPI_INT, winnerRank, MPI_COMM_WORLD);
+			}
+
+			// Mark E. Send updates of personal info.
+			MPI_Gather(&self, 9, MPI_INT, &receiveBuffer, 9, MPI_INT, FIELD_PROCESS, MPI_COMM_WORLD);
+		}
 		
 		
 		MPI_Barrier(MPI_COMM_WORLD);
 	}
+	MPI_Finalize();
 }
 
 
